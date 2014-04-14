be49585d64cb11d86ec9023b0019b6ced54ecb48
Calvin <CalvinLiu411@gmail.com>
2014-04-14 11:23:28 -0700

    Catalog change

--- src/java/simpledb/Catalog.java
+++ src/java/simpledb/Catalog.java
@@ -18,7 +18,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class Catalog {
 
-    private static class Table 
+    public static class Table 
     {
             public String name;
             public String primaryKey;
@@ -36,6 +36,7 @@ public class Catalog {
     Map<String, Integer> nameToId;
     Map<Integer, Table> idToTable;
     List<Integer> ids;
+    List<Table> list_of_tables;
 
     /**
      * Constructor.
@@ -46,8 +47,20 @@ public class Catalog {
         idToTable = new ConcurrentHashMap<Integer, Table>();
         nameToId = new ConcurrentHashMap<String, Integer>();
         ids = new ArrayList<Integer>();
+        list_of_tables = new ArrayList<Table>();
     }
 
+   /**
+    * Returns a list of tables in the Catalog
+    */
+    public List<Table> get_list_of_tables() {
+    	for(int i = 0; i < ids.size(); i++)
+    	{
+    		list_of_tables.add(idToTable.get(ids.get(i))); 
+    	}
+    	return list_of_tables;
+    }
+    
     /**
      * Add a new table to the catalog.
      * This table's contents are stored in the specified DbFile.
